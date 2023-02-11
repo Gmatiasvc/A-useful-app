2eb0f069a4ae3852c6615b18a48151a4a1b2737f
Gmatiasvc <gmatiasvc@hotmail.com>
2023-02-10 19:00:20 -0500

    added exit command

--- cli.py
+++ cli.py
@@ -1,7 +1,20 @@
 #imports
 import rich
+from rich.console import Console
+import argparse
+
+console = Console()
+
 #main
+def main():
+    console.print("A (kinda) useful app CLI enviroment [DevBuild 1]\n")
+    while True:
+        command = console.input("[deep_sky_blue3]$ ")
+        cmd = command.split(" ")
+        if cmd[0] in ["exit"]:
+            break
+
 
 #tests
-if __name__ == "__Main__":
-    pass
+if __name__ == "__main__":
+    main()
